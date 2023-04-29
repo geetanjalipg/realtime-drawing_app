5969ac675bb7b7754763c24c5033c0ad53c618be
geetanjalipg <81006006+geetanjalipg@users.noreply.github.com>
2023-04-29 10:32:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+noseX=0;
+noseY=0;
+difference=0;
+rightWristX=0;
+leftWristX=0;
+
+
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+
+
+    canvas = createCanvas(500, 500);
+    canvas.position(550, 150);
+    poseNet = ml5.poseNet(video, modeloaded);
+    poseNet.on('pose', gotPoses);
+
+}
+function modeloaded() {
+    console.log(('PoseNet Is Initialized'));
+}
+
+function draw() {
+background("#3c737d");
+document.getElementById("sqaure_side").innerHTML ="Width ad Height of a Square will be = " + difference;
+ fill("#5adba3");
+stroke("#5adba3");
+square(noseX,noseY,difference);
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log("noseX = "+  noseX +"noseY = " + noseY);
+        
+      leftWristX=results[0].pose.leftWrist.x;
+      rightWristX=results[0].pose.rightWrist.x;
+     difference=floor(leftWristX-rightWristX);
+     console.log("leftWristX = " + leftWristX + "rightWristX = "+rightWristX+ "difference = "+ difference);
+    }
+}
